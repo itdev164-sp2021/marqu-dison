b733c7d26885a6a21fb562a2e9eb446c0976c41c
Marquise Dison <disonm.gmatc.matc.edu>
2021-01-30 18:46:36 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog.`,
+    author: `Marquise Dison`,
+    contact: {
+      name: `Marquise Dison`,
+      company: `Blogs Inc.`,
+      address: `PO Box 4321`
+    }
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
